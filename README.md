04ac27837e45cf82fffb546ec3fbb2209811b678
guillaumefrd <guillaume-fradet@hotmail.fr>
2018-11-07 18:11:01 +0100

    sort the RDDs & refacto server.py

---
+++ README.md
@@ -0,0 +1,14 @@
+# Pyspark streaming on Twitter
+
+### Getting started
+
+1. Launch the server to stream the tweets. You must specify a word that will be in the streamed tweets (e.g. "Paris")
+
+```
+python server.py {word_in_tweet}
+```
+
+2. Run the jupyter notebook
+
+----
+*Credits: Felix Larrouy & Guillaume Fradet*
--- server.py
+++ server.py
@@ -1,15 +1,18 @@
 import tweepy
+import socket
+import json
+import sys
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
-from socket import socket
-import json
-import sys
 
 consumer_key = '8bwa6ory4xsBzOIJ2gAO2ukK2'
 consumer_secret = 'cEDvbXAa9DkLPWxutnTZpF2gptgruRRj3KhGMfkYdMqkqCfdqj'
 access_token = '1059729535053295617-SgThBTq7GRhA8bvqVxwatOBku3COBA'
 access_token_secret = '3F6qTH4Avtqus6HowNKZHE1epGtzPPtMAk7IRhFE20HyN'
+
+host = "localhost"    # Get local machine name
+port = 5555           # Reserve a port for streaming.
 
 class TweetsListener(StreamListener):
     def __init__(self, csocket):
@@ -23,14 +26,14 @@ class TweetsListener(StreamListener):
             tweet = json.loads(data)
             print(tweet['text'])
             self.client_socket.send(tweet['text'].encode('utf-8'))
-            return True
+            # return True
         except BaseException as e:
             print("Error on_data: %s" % str(e))
-        return True
+            return False
 
     def on_error(self, status):
         print(status)
-        return True
+        return False
 
 def sendData(c_socket):
     auth = OAuthHandler(consumer_key, consumer_secret)
@@ -38,14 +41,15 @@ def sendData(c_socket):
     twitter_stream = Stream(auth, TweetsListener(c_socket))
     twitter_stream.filter(track=[sys.argv[1]])
 
-s = socket()          # Create a socket object
-host = "localhost"    # Get local machine name
-port = 5555           # Reserve a port for streaming.
-s.bind((host, port))  # Bind to the port
 
-print("Listening on port: %s" % str(port))
-s.listen(5)           # Now wait for client connection.
-c, addr = s.accept()  # Establish connection with client.
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # socket object
+    s.bind((host, port)) # Bind to the port
+    print("Listening on port: %s" % str(port))
 
-print("Received request from: " + str(addr))
-sendData(c)
+    s.listen(1) # Now wait for 1 client connection.
+    conn, addr = s.accept() # Establish connection with client.
+    with conn:
+        print('Received request from:', addr)
+        while True:
+            sendData(conn)
+        print('Connection lost with:', addr)
